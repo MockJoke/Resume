5bec8976372bd3831c97959268800774a7dfd1f4
MockJoke <parvadesai404@gmail.com>
2023-11-17 23:57:11 +0530

    feat: Added functionality to switch theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
+
+  darkMode: "class",
+
   theme: {
     fontFamily: {
       systemui: ["system-ui", "sans-serif"],
