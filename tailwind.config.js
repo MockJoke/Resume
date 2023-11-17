ba4e2bd48c2d11a4235e9a8b15239dc3143b18f1
MockJoke <parvadesai404@gmail.com>
2023-11-17 23:57:42 +0530

    feat: Added dark theme colors in config

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,25 +10,26 @@ module.exports = {
       segoe: ["Segoe UI", "serif"],
       serif: ["DM Sans", "serif"],
     },
-    
+
     colors: {
-      "mainbg-color": "rgb(251, 251, 250)",
+      "mainbg-color": "rgb(249, 248, 244)",
+      "mainbg-color-dark": "rgb(30, 30, 30)",
       "font-color": "rgb(55, 53, 47)",
+      "font-color-dark": "rgb(235, 235, 235)",
       "link-color": "rgb(232, 28, 79)",
       "hover-color": "rgb(108, 117, 125)",
     },
 
     extend: {
       fontSize: {
-        "1.5xl" : "22px",
-        "2.75xl": "28px"
+        "1.5xl": "22px",
+        "2.75xl": "28px",
       },
 
       borderWidth: {
-        "1.5": "1.5px"
-      }
+        1.5: "1.5px",
+      },
     },
   },
   plugins: [],
-}
-
+};
